39c656baeee4e1cde8d5e36eaa57b72bd776d1e9
adwaver <adwaver4157@gmail.com>
2022-10-20 14:01:53 +0900

    feat: update detector function

--- aruco_marker.py
+++ aruco_marker.py
@@ -3,6 +3,7 @@
 import cv2
 import numpy as np
 import os
+import argparse
 # ArUcoのライブラリを導入
 aruco = cv2.aruco
 
@@ -11,15 +12,15 @@ dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
 image_dir = "images"
 pixel = 150
 offset = 10
-cnt = 9
 
-def generateArMarker():
+def generateArMarker(size=3):
 	# 白いブランク画像を生成
 	img = np.zeros((pixel + offset, pixel + offset), dtype=np.uint8)
 	img += 255
 
 	x_offset = y_offset = int(offset) // 2
 	# 9枚のマーカを作成する
+	cnt = size**2
 	for i in range(cnt):
 		# 150x150ピクセルで画像を作成
 		ar_image = aruco.drawMarker(dictionary, i, pixel, 3)
@@ -32,16 +33,17 @@ def generateArMarker():
 		
 		# ArUcoマーカの画像を結合
 		# 左端の時はそのまま水平方向用のbufferに代入
-		if (i % 3 == 0):
+		if (i % size == 0):
 			hconcat_img = rgb_img
 		# 右端まで達するまで水平用bufferにconcat
-		elif (i % 3 <= 2):
+		elif (i % size <= size-1):
 			hconcat_img = cv2.hconcat([hconcat_img, rgb_img])
 			# 右端まで行きかつ１段目の時は鉛直方向用bufferに代入
-			if (i % 3 == 2 and i // 3 == 0):
+			if (i % size == size-1 and i // size == 0):
 				vconcat_img = hconcat_img
+
 			# 右端まで行きかつ２段目以降の時は鉛直方向にconcat
-			elif (i % 3 == 2 and i // 3 > 0):
+			elif (i % size == size-1 and i // size > 0):
 				vconcat_img = cv2.vconcat([vconcat_img, hconcat_img])
 
 		# 1枚ごとのArUcoマーカを出力
@@ -49,9 +51,13 @@ def generateArMarker():
 		cv2.imwrite(file_path, rgb_img)
 
 	# 結合したArUcoマーカを出力
-	filename = "ar" + str(cnt) + ".png"
+	filename = f"{size**2}_qr.png"
 	file_path = os.path.join(image_dir, filename)
 	cv2.imwrite(file_path, vconcat_img)
         
 if __name__ == "__main__":
-    generateArMarker()
+	parser = argparse.ArgumentParser(description="Making aruco marker")
+	parser.add_argument("--size", type=int, default=3)
+
+	args = parser.parse_args()
+	generateArMarker(args.size)
--- estimator.py
+++ estimator.py
@@ -1,3 +1,18 @@
+import torch, detectoron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+# import some common libraries
+import numpy as np
+import os, json, cv2, random
+
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog, DatasetCatalog
+
 def convert_oracle_to_image_pos(image_pos_dict, target_oracle_pos=[8, 6], table_size=[15, 15]):
     cornerUL = image_pos_dict[0]
     cornerDR = image_pos_dict[3]
@@ -7,6 +22,21 @@ def convert_oracle_to_image_pos(image_pos_dict, target_oracle_pos=[8, 6], table_
     print(diff)
     print(target_pos_int)
     # target_image_pos = [100, 100]
+    return target_pos_int
+
+def detect_image_pos(image):
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+    predictor = DefaultPredictor(cfg)
+    outputs = predictor(image)
+    v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+    out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+    cv2.imwrite('results/test.jpg', out.get_image()[:, :, ::-1])
+    # cv2_imshow(out.get_image()[:, :, ::-1])
+    target_image_pos = [100, 100]
+    target_pos_int = [int(x) for x in target_image_pos]
     return target_pos_int
 
 def convert_image_to_table_pos(image_pos_dict, table_pos_dict, target_image_pos, table_size=[15, 15]):
